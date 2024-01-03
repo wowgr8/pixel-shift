0b4fba03959dae30731bc1c7f0e55176bafef544
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-02 20:33:45 -0800

    rename file

---
+++ src/Control.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+interface ControlProps {
+  
+}
+
+const Control: FC<ControlProps> = ({  }) => {
+  return (
+    <div>
+      Control
+    </div>
+  )
+}
+
+export default Control;
