3e669cd678d224a893dd2fdeba7b86306c809ab2
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-03 15:59:32 -0800

    add useState hook, tiles useState and imageScramble fn - WIP

--- src/Control.tsx
+++ src/Control.tsx
@@ -1,13 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import R34 from './assets/img/R34.jpeg'
 
 interface ControlProps {
   
 }
 
 const Control: FC<ControlProps> = ({  }) => {
+  const [tiles, setTiles] = useState<number[][]>([[]]);
+
+  const imageScramble = () => {
+    R34
+  }
+
   return (
     <div>
-      Control
+      {/* original image */}
+      <img src={R34} />
+      {/* scrambled image */}
+      
     </div>
   )
 }
