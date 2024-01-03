d219c885337b61b4402d4e7370386d34cf488077
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-02 20:28:30 -0800

    rename file

---
+++ src/Ctrl.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface CtrlProps {
+  
+}
+
+const Ctrl: FC<CtrlProps> = ({  }) => {
+  return (
+    <div>
+      Ctrl
+    </div>
+  )
+}
+
+export default Ctrl;
