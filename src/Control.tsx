3b51fed8afef8b45edddf13eca2074b760695dea
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2024-01-15 18:48:14 -0800

    add loops that assigns numbers to each cell in grid by column and row position, add numbered cells to Tiles state.

--- src/Control.tsx
+++ src/Control.tsx
@@ -16,6 +16,16 @@ const Control: FC<ControlProps> = ({  }) => {
     const cols = 3;
     const tileWidth = 300;
     const tileHeight = 300;
+
+    const initialTiles: number[][] = []; // creates empty 2D array to store grid of numbers
+    for (let row = 0; row < rows; row++) { // loop iterates through each row of grid
+      const rowArray: number[] = []; //new empty array created for each row
+      for (let col = 0; col < cols; col++) { // neested loop iterates through each column in current row
+        rowArray.push(row * cols + col); //gives unique number for each cell in the grid, based on row and column position, then push to rowArray
+      }
+      initialTiles.push(rowArray); //each column in current row is pushed to tilesArray - completes one row of the grid
+    }
+    setTiles(initialTiles);
   }
 
   return (
